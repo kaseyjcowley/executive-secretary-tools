7ebb6df96c8a3cf97ca627fe339aed284295d2f5
Kasey Cowley <kcowley1988@gmail.com>
2023-11-10 21:12:25 -0700

    Include formatted date in timezone and add border for before/after church divide

--- src/components/InterviewsTable.tsx
+++ src/components/InterviewsTable.tsx
@@ -1,4 +1,6 @@
-import formatDate from "date-fns/format";
+import { format as formatDate } from "date-fns-tz";
+import getHours from "date-fns/getHours";
+import getMinutes from "date-fns/getMinutes";
 
 import { CallingTrelloCard, InterviewTrelloCard } from "@/requests/cards";
 import { getMemberName } from "@/requests/members";
@@ -16,28 +18,41 @@ export const InterviewsTable = ({ memberId, interviews }: Props) => {
         <tr className="border border-slate-500">
           <td className="p-2 font-bold">{getMemberName(memberId)}</td>
         </tr>
-        {interviews.map((card) => (
-          <tr key={card.name} className="odd:bg-white even:bg-slate-50">
-            <td className="border border-slate-500 p-2">{card.name}</td>
-            <td className="border border-slate-500 p-2">
-              {formatDate(new Date(card.due), "h:mmaaa")}
-            </td>
-            <td className="border border-slate-500 p-2">
-              {(() => {
-                switch (card.kind) {
-                  case "calling": {
-                    return card.stage === CallingStage.needsCallingExtended
-                      ? `Calling as ${card.calling}`
-                      : `Setting apart as ${card.calling}`;
+        {interviews.map((card) => {
+          const hour = getHours(new Date(card.due));
+          const minutes = getMinutes(new Date(card.due));
+          return (
+            <tr
+              key={card.name}
+              className={`odd:bg-white even:bg-slate-50 ${
+                hour === 14 && minutes === 0
+                  ? `border-t-4 border-slate-900`
+                  : ``
+              }`}
+            >
+              <td className="border border-slate-500 p-2">{card.name}</td>
+              <td className="border border-slate-500 p-2">
+                {formatDate(new Date(card.due), "h:mmaaa", {
+                  timeZone: "America/Denver",
+                })}
+              </td>
+              <td className="border border-slate-500 p-2">
+                {(() => {
+                  switch (card.kind) {
+                    case "calling": {
+                      return card.stage === CallingStage.needsCallingExtended
+                        ? `Calling as ${card.calling}`
+                        : `Setting apart as ${card.calling}`;
+                    }
+                    case "interview": {
+                      return card.labels?.name;
+                    }
                   }
-                  case "interview": {
-                    return card.labels?.name;
-                  }
-                }
-              })()}
-            </td>
-          </tr>
-        ))}
+                })()}
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
