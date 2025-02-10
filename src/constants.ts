372e7d81542c2a2968c7c5b532d39d9778873fec
Kasey Cowley <kcowley1988@gmail.com>
2025-02-09 20:11:08 -0700

    Update constants with new bishopric members

--- src/constants.ts
+++ src/constants.ts
@@ -7,21 +7,21 @@ enum BishopricMember {
 
 export const BishopricMemberId: Record<BishopricMember, string> = {
   [BishopricMember.bishop]: "5f5b72d54777a21de18c6ac5",
-  [BishopricMember.firstCounselor]: "575498b14e0af55e0d2a90d1",
-  [BishopricMember.secondCounselor]: "617ab001171dce606829c4a0",
+  [BishopricMember.firstCounselor]: "5552302a4bb771aa0a2532e2",
+  [BishopricMember.secondCounselor]: "67a938bda1e6bd8da53408ba",
   [BishopricMember.executiveSecretary]: "5a837d172c1860b067ef60c8",
 } as const;
 
 export enum SlackChannelId {
   automationTesting = "C04R54CHA78",
-  bishopric = "C01AFJHRJ49",
+  bishopric = "C08CE1P0NBF",
   wardCouncil = "C01A3032RB4",
 }
 
 export const BishopricSlackMemberIds: Record<BishopricMember, string> = {
   [BishopricMember.bishop]: "U01ASQSJ04Q",
-  [BishopricMember.firstCounselor]: "U01BTJHB2Q4",
-  [BishopricMember.secondCounselor]: "U026TTPBT4H",
+  [BishopricMember.firstCounselor]: "U02RLUSKGQL",
+  [BishopricMember.secondCounselor]: "U07T96ABDDX",
   [BishopricMember.executiveSecretary]: "U01B1LT9999",
 } as const;
 
@@ -35,7 +35,7 @@ export const BishopricMemberName: Record<
   string
 > = {
   "5f5b72d54777a21de18c6ac5": "Paul Hill",
-  "575498b14e0af55e0d2a90d1": "Dan Boles",
-  "617ab001171dce606829c4a0": "Steve Nielsen",
+  "5552302a4bb771aa0a2532e2": "Chris Davis",
+  "67a938bda1e6bd8da53408ba": "Will Stewart",
   "5a837d172c1860b067ef60c8": "Kasey Cowley",
 } as const;
