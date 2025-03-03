55e416af279b64ea0838a12f1986ac87b52f15ad
Kasey Cowley <kcowley1988@gmail.com>
2025-03-02 22:13:18 -0700

    Add handler for Slack action

---
+++ src/utils/email.ts
@@ -0,0 +1,38 @@
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
+import Mail from "nodemailer/lib/mailer";
+const OAuth2 = google.auth.OAuth2;
+
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.GMAIL_CLIENT_ID,
+    process.env.GMAIL_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.GMAIL_REFRESH_TOKEN,
+  });
+
+  const accessToken = await oauth2Client.getAccessToken();
+
+  const transporter = nodemailer.createTransport({
+    // @ts-expect-error
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: "kcowley1988@gmail.com",
+      accessToken,
+      clientId: process.env.GMAIL_CLIENT_ID,
+      clientSecret: process.env.GMAIL_CLIENT_SECRET,
+      refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+    },
+  });
+
+  return transporter;
+};
+
+export const sendEmail = async (emailOptions: Mail.Options) => {
+  let emailTransporter = await createTransporter();
+  await emailTransporter.sendMail(emailOptions);
+};
